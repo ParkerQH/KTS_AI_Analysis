690b3ab75c4dbba945b4cdea2fbe971ac84f2e49
ParkerQH <blueskyaoa7@gmail.com>
2025-07-18 08:40:38 +0900

    저장 데이터 일부 수정

--- AI_Analysis.py
+++ AI_Analysis.py
@@ -9,6 +9,7 @@ from firebase_admin import storage, firestore
 import YOLOv11.YOLO as YOLO, YOLOv11.geocoding as geocoding, MediaPipe.lstm_Analysis as lstm_p1
 
 
+# firestore 이미지 다운로드
 def download_image(url):
     """이미지 URL에서 이미지를 다운로드해 numpy array로 반환"""
     resp = requests.get(url, stream=True)
@@ -20,13 +21,33 @@ def download_image(url):
         print(f"🚫 이미지 다운로드 실패: {url}")
         return None
 
+
+# GPS 정도 바탕 지번주소 추출
+def find_adress(doc_id):
+    # 신고 정보 중 GPS 가져와 지번주소 추출
+    db_fs = firestore.client()
+    doc_ref = db_fs.collection("Report").document(doc_id)
+    doc = doc_ref.get()
+    if doc.exists:
+        doc_data = doc.to_dict()
+        gps_info = doc_data.get("gpsInfo")
+    if gps_info:
+        lat_str, lon_str = gps_info.strip().split()
+        lat = float(lat_str)
+        lon = float(lon_str)
+        parcel_addr = geocoding.reverse_geocode(lat, lon, os.getenv("VWorld_API"))
+        return lat, lon, parcel_addr
+
+
 # firebase 데이터 저장 메소드
-def save_conclusion(doc_id, date, user_id, violation, result, aiConclusion=None, detectedBrand=None,
-    confidence=None, imageUrl=None, reportImgUrl=None, region=None,  gpsInfo=None):
-    
+def save_conclusion(
+    doc_id, date, user_id, violation, result, region, gpsInfo, imageUrl, reportImgUrl, 
+    aiConclusion=None, detectedBrand=None, confidence=None
+):
+
     db_fs = firestore.client()
     full_doc_id = f"conclusion_{doc_id}"
-    
+
     # 저장할 데이터
     conclusion_data = {
         "date": date,
@@ -34,24 +55,21 @@ def save_conclusion(doc_id, date, user_id, violation, result, aiConclusion=None,
         "aiConclusion": aiConclusion or [],
         "violation": violation,
         "result": result,
+        "region": region,
+        "gpsInfo": gpsInfo,
         "imageUrl": imageUrl,
-        "reportImgUrl": reportImgUrl or imageUrl
+        "reportImgUrl": reportImgUrl or imageUrl,
     }
-    
+
     # 브랜드
     if detectedBrand:
         conclusion_data["detectedBrand"] = detectedBrand
-    # conf 
+    # conf
     if confidence is not None:
         conclusion_data["confidence"] = confidence
-    # gps 정보가 있는 경우
-    if gpsInfo is not None:
-        conclusion_data["gpsInfo"] = gpsInfo
-    # 지번 주소
-    if region:
-        conclusion_data["region"] = region
 
     db_fs.collection("Conclusion").document(full_doc_id).set(conclusion_data)
+
 
 def process_image(image_url, date, user_id, violation, doc_id):
     print(f"🔥 이미지 처리 시작: {image_url}")
@@ -100,9 +118,11 @@ def process_image(image_url, date, user_id, violation, doc_id):
         )
         if helmet_detected:
             YOLO.draw_boxes(helmet_results, image, (0, 0, 255), "Helmet")
+            print("✅ 헬멧 감지")
             # cv2.imwrite(f"output/annotated_{doc_id}.jpg", image)
         else:
             traffic_violation_detection.append("헬멧 미착용")
+            print("🚫 헬멧 미착용")
 
         # 분석 이미지 저장 (Firebase Storage)
         bucket = storage.bucket()
@@ -115,28 +135,22 @@ def process_image(image_url, date, user_id, violation, doc_id):
         conclusion_url = conclusion_blob.public_url
 
         # 신고 정보 중 GPS 가져와 지번주소 추출
-        db_fs = firestore.client()
-        doc_ref = db_fs.collection("Report").document(doc_id)
-        doc = doc_ref.get()
-        if doc.exists:
-            doc_data = doc.to_dict()
-            gps_info = doc_data.get("gpsInfo")
-        if gps_info:
-            lat_str, lon_str = gps_info.strip().split()
-            lat = float(lat_str)
-            lon = float(lon_str)
-            parcel_addr = geocoding.reverse_geocode(lat, lon, os.getenv("VWorld_API"))
+        lat, lon, parcel_addr = find_adress(doc_id)
 
         # Firestore에 저장될 내용
         save_conclusion(
-            doc_id=doc_id, date=date, user_id=user_id, violation=violation,
-            result="미확인", aiConclusion=traffic_violation_detection,
+            doc_id=doc_id,
+            date=date,
+            user_id=user_id,
+            violation=violation,
+            result="미확인",
+            aiConclusion=traffic_violation_detection,
             detectedBrand=top_brand_class,
             confidence=top_helmet_confidence,
             gpsInfo=f"{lat} {lon}",
             region=parcel_addr,
             imageUrl=conclusion_url,
-            reportImgUrl=image_url
+            reportImgUrl=image_url,
         )
 
         print(f"✅ 분석된 사진 url : {conclusion_url}\n")
@@ -144,10 +158,20 @@ def process_image(image_url, date, user_id, violation, doc_id):
     elif kickboard and person:
         print("🛑 사진 속 사람은 보행자로 판단됩니다. 자동 반려처리 진행됩니다.\n")
 
+        # 신고 정보 중 GPS 가져와 지번주소 추출
+        lat, lon, parcel_addr = find_adress(doc_id)
+
         save_conclusion(
-            doc_id=doc_id, date=date, user_id=user_id, violation=violation,
-            result="반려", aiConclusion=traffic_violation_detection,
-            imageUrl=image_url, reportImgUrl=image_url
+            doc_id=doc_id,
+            date=date,
+            user_id=user_id,
+            violation=violation,
+            result="반려",
+            aiConclusion=traffic_violation_detection,
+            gpsInfo=f"{lat} {lon}",
+            region=parcel_addr,
+            imageUrl=image_url,
+            reportImgUrl=image_url,
         )
 
         print(f"❌ 반려된 사진 url : {image_url}\n")
@@ -155,10 +179,20 @@ def process_image(image_url, date, user_id, violation, doc_id):
     else:
         print("🛑 킥보드 혹은 사람을 감지하지 못했습니다. 자동 반려처리 진행됩니다.\n")
 
+        # 신고 정보 중 GPS 가져와 지번주소 추출
+        lat, lon, parcel_addr = find_adress(doc_id)
+
         save_conclusion(
-            doc_id=doc_id, date=date, user_id=user_id, violation=violation,
-            result="반려", aiConclusion=traffic_violation_detection,
-            imageUrl=image_url, reportImgUrl=image_url
+            doc_id=doc_id,
+            date=date,
+            user_id=user_id,
+            violation=violation,
+            result="반려",
+            aiConclusion=traffic_violation_detection,
+            gpsInfo=f"{lat} {lon}",
+            region=parcel_addr,
+            imageUrl=image_url,
+            reportImgUrl=image_url,
         )
 
         print(f"❌ 반려된 사진 url : {image_url}\n")
